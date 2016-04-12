95a62e98e94e92b2e846bb48b31c00b0fc18efdf
joshuaavalon <joshuaavalon@gmail.com>
2016-04-12 18:12:15 +0800

    Update

--- Assets/Scripts/Gui/MoveForward.cs
+++ Assets/Scripts/Gui/MoveForward.cs
@@ -12,11 +12,18 @@ namespace Assets.Scripts.Gui
         private void Start()
         {
             WrapJump.SetActive(true);
-            StartCoroutine(Wait(3.5F));
+            StartCoroutine(Wait(10F));
+            StartCoroutine(WaitCanves(3.5F));
         }
 
-
         IEnumerator Wait(float time)
+        {
+            yield return new WaitForSeconds(time);
+            WrapJump.SetActive(false);
+            Canvas.SetActive(true);
+        }
+
+        IEnumerator WaitCanves(float time)
         {
             yield return new WaitForSeconds(time);
             WrapJump.SetActive(false);
