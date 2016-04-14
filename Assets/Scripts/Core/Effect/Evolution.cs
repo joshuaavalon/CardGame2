c8bf2fc835aa1437839d68fee6ebceb2f6e4a913
mcsoaa <mcsoaa@connect.ust.hk>
2016-04-14 12:00:53 +0800

    Card 3
    Card 3

---
+++ Assets/Scripts/Core/Effect/Evolution.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.Core.Message;
+using Assets.Scripts.Core.Statistics;
+using Assets.Scripts.Infrastructure.EventAggregator;
+
+namespace Assets.Scripts.Core.Effect
+{
+    //At each Main Phase, Gain +1/+1.
+    public class Evolution : BaseEffect, IHandle<PhaseStartMessage>
+    {
+        public int AttackBonus;
+        public int HpBonus;
+
+        public override void OnEnter()
+        {
+            Parent.Parent.Game.Subscribe(this);
+        }
+        
+        public void Handle(PhaseStartMessage message)
+        {
+            var phase = message.Phase;
+            if (phase.GetParent() != Parent.Parent.Type || phase.GetName() != "Main Phase") return;
+            Parent.SetStats(CardStatsType.Atk, Parent.GetStats(CardStatsType.Atk) + AttackBonus);
+            Parent.SetStats(CardStatsType.Hp, Parent.GetStats(CardStatsType.Hp) + HpBonus);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Effect/FriendlyFire.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Assets.Scripts.Core.Statistics;
+
+namespace Assets.Scripts.Core.Effect
+{
+    //When the battlefield has the same type of spacecraft, gain +1/+1.
+    public class FriendlyFire : BaseEffect
+    {
+        public int AttackBonus;
+        public int HpBonus;
+
+        public override void OnEnter()
+        {
+            var game = Parent.Parent.Game;
+            var player = game.GetPlayer(Parent.Parent.Type);
+            var cards = player.Battlefield;
+            foreach (var card in cards)
+            {
+                if (card == Parent) break;
+                card.SetStats(CardStatsType.Atk, Parent.GetStats(CardStatsType.Atk) + AttackBonus);
+                card.SetStats(CardStatsType.Hp, Parent.GetStats(CardStatsType.Hp) + HpBonus);
+            }
+        }
+    }
+}
