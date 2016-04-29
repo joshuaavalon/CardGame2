1cf381bc3261a516b540b59ed0c4784cbe971858
mcsoaa <mcsoaa@connect.ust.hk>
2016-04-29 15:04:46 +0800

    Card Draw
    Card Draw

--- Assets/Scripts/Core/Effect/DrawCard.cs
+++ Assets/Scripts/Core/Effect/DrawCard.cs
@@ -6,10 +6,12 @@
         public int DrawCardNumber ;
         public override void OnEnter()
         {
-            var player = PhotonNetwork.isMasterClient ? PlayerType.Player : PlayerType.Opponent;
+            var player = Parent.Parent.Type;
             if (player != PlayerType.Player) return;
-            for (var i=0; i< DrawCardNumber ; i++)
-                Parent.Parent.Game.DrawCard(PlayerType.Player);
+            for (var i = 0; i < DrawCardNumber; i++)
+            {
+                Parent.Parent.Game.DrawCard(player);
+            }
         }
     }
 }
